9565c92e69a295c957861327c38330e3bb3ada3a
SupaSibs <92636078+SupaSibs@users.noreply.github.com>
2022-04-25 01:04:29 -0700

    Update types.js

--- types.js
+++ types.js
@@ -1,6 +1,12 @@
+function createType(name, type) {
+ return type 
+}
+
 function checkType(var, type) {
 if (typeof var !== type) {
 console.log("Types dont match");
 throw new Error("Type is wrong")
 }
 }
+
+export { createType, checkType}
